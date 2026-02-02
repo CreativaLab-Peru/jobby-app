4361bca75cf40fe40b139574d93e575e720c5c2c
Edward Melendez <edward.melendez@limber.io>
2026-02-02 09:21:39 -0500

    fix: change userId and type for v3

--- features/credits/actions/get-user-balance.ts
+++ features/credits/actions/get-user-balance.ts
@@ -1,12 +1,24 @@
 import {prisma} from "@/lib/prisma";
+import {CreditBalanceType} from "@prisma/client";
+
+export type GetUserBalanceParams = {
+  userId: string;
+  type: CreditBalanceType;
+};
 
 /**
  * CONSULTA DE SALDO
  * Rápida y sin transacciones pesadas.
  */
-export const getUserBalance = async (userId: string) => {
+export const getUserBalance = async (body: GetUserBalanceParams) => {
+  const {userId, type} = body;
   const balance = await prisma.userCreditBalance.findUnique({
-    where: { userId },
+    where: {
+      userId_type: {
+        userId,
+        type
+      }
+    },
     select: { amount: true }
   });
 

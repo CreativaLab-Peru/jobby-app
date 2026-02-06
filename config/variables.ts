0474a03e37c2a7a536ee757b5ab53c793240563b
Edward Melendez <edward.melendez@limber.io>
2026-02-06 03:01:44 -0500

    upd: CHANGELOG.MD

---
+++ config/variables.ts
@@ -0,0 +1,14 @@
+type Environment = "development" | "production";
+
+const ENV = process.env.NODE_ENV as Environment;
+
+const VARIABLES = {
+  development: {
+    BETTER_AUTH_URL: "http://localhost:3001",
+  },
+  production: {
+    BETTER_AUTH_URL: "https://www.joinlevely.com",
+  },
+} as const;
+
+export const VARS = VARIABLES[ENV ?? "development"];

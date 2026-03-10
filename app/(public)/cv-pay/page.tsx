9afd210a59e4b9373201596f47370198af15d3f2
Edward Melendez <edward.melendez@limber.io>
2026-03-10 17:54:00 -0500

    upd: public pages

--- app/(public)/cv-pay/page.tsx
+++ app/(public)/cv-pay/page.tsx
@@ -1,15 +1,7 @@
-import {HotSaleSection} from "@/components/ui/app/public/cv-builder/hot-sale-section";
-import { CreditPackModal } from "@/features/credits/components/credit-pack-modal";
-import { getSession } from "@/features/authentication/actions/get-session";
+import {CVPayScreen} from "@/components/public/screens/cv-pay-screen";
+import {getCurrentUser} from "@/features/share/actions/get-current-user";
 
 export default async function CVPayPage() {
-  const session = await getSession();
-  const sessionUser = session.success ? session.user : null;
-
-  return (
-    <>
-      <HotSaleSection sessionUser={sessionUser} />
-      <CreditPackModal />
-    </>
-  )
+  const currentUser = await getCurrentUser();
+  return <CVPayScreen user={currentUser}/>
 }

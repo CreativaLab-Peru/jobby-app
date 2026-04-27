d384a97eb12aff2b549ac32046027147d8608afe
Jhon Jesus <jhonjesusquispemachaca@gmail.com>
2026-04-27 17:04:28 -0500

    fix: 86ah46hy0 fixed newsletter in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const footerLinks = {
   Producto: [
     { name: "CV Builder", href: "/cv-builder" },
     // { name: "Career Accelerator", href: "/career-accelerator" },
-    { name: "News letter", href: "/newsletter" },
+    { name: "Newsletter", href: "/newsletter" },
     //{ name: "Resources", href: "/resources" },
   ],
   Empresa: [
